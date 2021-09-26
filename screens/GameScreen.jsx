8a107cc71cdabfdec515abc952471327393b76f8
nickjiang2378 <41227221+nickjiang2378@users.noreply.github.com>
2021-09-26 15:12:38 -0700

    edits

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -5,22 +5,31 @@ import { styles } from "../constants/Styles";
 import { nameToPic } from "../constants/Constants";
 import { useEffect } from "react";
 import { shuffle } from "../utils/ArrayUtils";
+import { ScreenStackHeaderBackButtonImage } from "react-native-screens";
 const names = Object.keys(nameToPic);
 
 export default function GameScreen() {
   // TODO: Declare and initialize state variables here, using "useState".
+  const [correctChoices, setCorrectChoices] = useState(0);
+  const [rounds, setRounds] = useState(0);
+  const [rightName, setRightName] = useState();
+  const [currentImage, setCurrentImage] = useState();
+  const [currentNames, setCurrentNames] = useState(["NA", "NA", "NA", "NA"]);
 
   // State for the timer is handled for you.
   const [timeLeft, setTimeLeft] = useState(5000);
 
-  // Called by the timer every 10 seconds
+  // Called by the timer every 10 miliseconds
   const countDown = () => {
     if (timeLeft > 0) {
       // Time still left
       // TODO: update appropriate state variables
+      setTimeLeft(timeLeft - 10)
     } else {
       // Time has expired
       // TODO: update appropriate state variables
+      getNextRound()
+      setRounds(rounds + 1)
     }
   };
 
@@ -28,6 +37,7 @@ export default function GameScreen() {
   // It updates state to present a new member & name options.
   const getNextRound = () => {
     // Fetches the next member name to guess.
+    console.log("Getting next round")
     let correct = names[Math.floor(Math.random() * names.length)];
     let correctName = nameToPic[correct][0];
     let correctImage = nameToPic[correct][1];
@@ -44,17 +54,36 @@ export default function GameScreen() {
     nameOptions = shuffle(nameOptions);
 
     // TODO: Update state here.
+    setRightName(correctName);
+    setCurrentNames(nameOptions)
+    setCurrentImage(correctImage)
 
     setTimeLeft(5000);
   };
 
   // Called when user taps a name option.
   // TODO: Update correct # and total # state values.
-  const selectedNameChoice = (index) => {};
+  const selectedNameChoice = (index) => {
+    console.log(index)
+    if (currentNames[index] == rightName) {
+      setCorrectChoices(correctChoices + 1)
+    }
+    getNextRound()
+    setRounds(rounds + 1)
+  };
 
   useEffect(() => {
     /* TODO: Call the countDown() method every 10 milliseconds */
+    const interval = setInterval(() => {
+      countDown();
+    }, 10); 
+    return () => clearInterval(interval);
   });
+
+  useEffect(() => {
+    console.log("Initializing round")
+    getNextRound()
+  }, [])
 
   // TODO: Finish this useEffect() hook such that we automatically
   // get the next round when the appropriate state variable changes.
@@ -66,7 +95,13 @@ export default function GameScreen() {
     const j = i;
     nameButtons.push(
       // TODO: Implement a Button/Pressable type that shows a name choice, and implement the functionality when a user press on it
-      // Hint: Most functionality is already taken care of by one of the functions already defined
+      // Hint: Most functionality is already taken care of by one of the functions already defined      
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => selectedNameChoice(i)}
+      >
+        <Text style={styles.buttonText}>{currentNames[i]}</Text>
+      </TouchableOpacity>
     );
   }
 
@@ -74,7 +109,14 @@ export default function GameScreen() {
 
   // Style & return the view.
   return (
-    <View>
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={currentImage}
+      ></Image>
+      <Text style={styles.scoreText}>Current Score: {correctChoices} / {rounds}</Text>
+      <Text style={styles.timerText}>Time Left: {timeRemainingStr}</Text>
+      {nameButtons}
       {/* TODO: Build out your UI using Text and Image components. */}
       {/* Hint: What does the nameButtons list above hold? 
           What types of objects is this list storing?
@@ -82,3 +124,8 @@ export default function GameScreen() {
     </View>
   );
 }
+/*
+      <Image
+        style={styles.image}
+        source={nameToPic[correctName][1]}
+      ></Image> */
